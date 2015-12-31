769e20e8f33d0f252547ceed63916eebbc4b2d1e
mgoelzer <mike@goelzer.com>
2015-12-31 14:18:14 -0800

    Added event listening and container creation

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # swarmlistener
+
+Do this first:
+```
+export DOCKER_HOST=tcp://192.168.33.11:3375     # point at swarm master
+```
--- swarmlistener.go
+++ swarmlistener.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/samalba/dockerclient"
 	"log"
 	"os"
@@ -9,7 +10,10 @@ import (
 )
 
 func eventCallback(e *dockerclient.Event, ec chan error, args ...interface{}) {
-	log.Println(e)
+	log.Printf(">> Event (Id=%v):\n", e.Id)
+	log.Printf(">>     Status:  %v\n", e.Status)
+	log.Printf(">>     From:    '%v'\n", e.From)
+	log.Printf(">>     Time:    %v\n", e.Time)
 }
 
 var (
@@ -26,6 +30,13 @@ func waitForInterrupt() {
 }
 
 func main() {
+	// Example input data:
+	serviceName := "nginx"
+	serviceImage := "nginx"
+	serviceCmd := []string{""}
+	serviceCnt := 3
+
+	// DOCKER_HOST should point to Swarm master, e.g., "tcp://192.168.33.11:3375"
 	docker, err := dockerclient.NewDockerClient(os.Getenv("DOCKER_HOST"), nil)
 	if err != nil {
 		log.Fatal(err)
@@ -34,6 +45,32 @@ func main() {
 	client = docker
 
 	client.StartMonitorEvents(eventCallback, nil)
+
+	// Start Service
+	containerIds := make([]string, serviceCnt)
+	for i := 0; i < serviceCnt; i++ {
+		containerConfig := &dockerclient.ContainerConfig{
+			Image: serviceImage,
+			Cmd:   serviceCmd,
+		}
+		containerName := fmt.Sprintf("%s-%02d", serviceName, i)
+		fmt.Println(containerName)
+		_ = containerConfig
+		containerId, err := docker.CreateContainer(containerConfig, containerName, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+		containerIds[i] = containerId
+	}
+	for _,containerId := range containerIds {
+		fmt.Printf("Started %v\n",containerId)
+	}
+
+	defer func() {
+		// Cleanup Service
+		fmt.Printf("Cleaning up...\n")
+		// TODO
+	}()
 
 	waitForInterrupt()
 }
